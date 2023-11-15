282b866aa9df457df303a707cd50261440da3917
Gideon <kipkerring.gedion@gmail.com>
2023-11-15 13:41:45 +0300

    accepts best match for the languages

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Basic Flask App"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config:
+    """has languanges english and french"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object("1-app.Config")
+
+
+@babel.localeselector
+def get_locale():
+    """determines the best match with our supported languages"""
+    return request.accept_languages.best_match(LANGUAGES)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def welcome_to_holberton():
+    """renders templates"""
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
