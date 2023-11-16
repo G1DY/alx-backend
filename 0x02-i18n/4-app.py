dcf93b94665cce354efd66c498ff08fd7f35ae14
Gideon <kipkerring.gedion@gmail.com>
2023-11-16 12:42:58 +0300

    4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python3
 """Flask app that implements babel library"""
 from flask import Flask, render_template, request
-from flask_babel import gettext, ngettext, Babel, get_locale
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
 
-class Config():
+
+class Config:
     """has languanges english and french"""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    BABEL_TRANSLATION_DIRECTORIES = 'translations'
+    BABEL_TRANSLATION_DIRECTORIES = "translations"
 
 
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config["BABEL_DEFAULT_LOCALE"] = "en"
 
 
+@babel.localeselector
 def get_locale():
     """determine the best match with our supported languages."""
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
+    locale = request.args.get("locale")
+    if locale and locale in app.config["LANGUAGES"]:
         return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel.init_app(app, locale_selector=get_locale)
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def hello_world():
     """renders templates"""
-    return render_template('4-index.html')
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
