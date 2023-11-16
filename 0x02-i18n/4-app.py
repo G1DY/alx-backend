22e5b9f5ec1935b47b2432329e24af8dca5576c2
Gideon <kipkerring.gedion@gmail.com>
2023-11-16 12:57:23 +0300

    4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app that implements babel library"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 babel = Babel(app)
